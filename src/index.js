8eca00f932d6c926441e0ac073ac2410218e6bc9
Samanway Ghatak <samanwayghatak@gmail.com>
2023-04-16 20:52:10 +0530

    Added SignIn component from material ui.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignIn from './SignIn';
 
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/signin', element: <SignIn /> }
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
