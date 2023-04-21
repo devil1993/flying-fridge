bf6bee3c918e1b5a7665ed71375d4fec8a54db42
Sam Ghatak <samanwayghatak@gmail.com>
2023-04-21 07:16:02 +0000

    Added firebase to project and added the service structure.

---
+++ src/FirebaseService.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3zcVKb6WbJsugOoMJYBE2GeTS6DHU3nU",
+  authDomain: "flying-fridge.firebaseapp.com",
+  projectId: "flying-fridge",
+  storageBucket: "flying-fridge.appspot.com",
+  messagingSenderId: "709977221159",
+  appId: "1:709977221159:web:ed256516db1d14493a8602"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+class FirebaseLoginService{
+    register(){
+
+    }
+    logIn(){
+
+    }
+    logOut(){
+
+    }
+}
+
+class FirebaseGratitudeService{
+
+}
+
+export default { FirebaseGratitudeService, FirebaseLoginService }
