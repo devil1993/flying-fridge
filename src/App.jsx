454d4577c583528167c71360070bc03c2daa5992
Sam Ghatak <samanwayghatak@gmail.com>
2023-04-27 13:37:04 +0000

    Creating basic overlay of dashboard.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import EditThanks from './DashBoard/EditThanks';
 import Published from './Published/Published';
 import { RouterProvider, createBrowserRouter} from 'react-router-dom';
 import ErrorPage from './Commons/ErrorPage';
+import { Box, Button, Card, CardActions, CardContent, Container, Grid, Input, Stack, TextField, Typography } from '@mui/material';
+import { Save, FileUpload } from '@mui/icons-material'
 
 const router = createBrowserRouter([
   {
@@ -30,9 +32,67 @@ function App() {
 
 function Hi(){
   return (
-    <div>
-      <h1>Todo: A thank you app here.</h1>
-    </div>
+    <>
+      <Container maxWidth="xl" >
+        <Grid container spacing={2} >
+            <Grid item md={12} sm={12} xs={12} lg={4}>
+              <Box sx={{
+                color: 'black'
+              }}>
+                <Typography variant='h3'>Welcome Samanway</Typography>
+              </Box>
+            </Grid>
+            <Grid item md={12} sm={12} xs={12} lg={8}>
+                <Card sx={{ minWidth: 275 }} elevation={4}>
+                  <CardContent>
+                    <Stack direction="row" spacing={2}>
+                      <Stack width="75%" spacing={2}>
+                        <TextField id="outlined-basic" label="Name" variant="outlined" />
+                        <TextField id="outlined-basic" label="Description" variant="outlined" />
+                        <TextField id="outlined-basic" label="Image URL" variant="outlined" />
+                        <Button 
+                            variant="contained"
+                            component="label"
+                        > 
+                            <Typography padding={2}>Upload Image</Typography>
+                            <FileUpload />
+                            <input
+                              type="file"
+                              hidden
+                            />
+                        </Button>
+                        {/* <Input type='file' id="outlined-basic" label="Image" variant="outlined" /> */}
+                      </Stack>
+                      <Box width="25%" sx={{border: "1px solid red"}}></Box>
+                    </Stack>
+                  </CardContent>
+                  <CardActions sx={{
+                    alignSelf: "stretch",
+                    display: "flex",
+                    justifyContent: "flex-end",
+                    alignItems: "flex-start",
+                    marginRight: 5
+                  }}>
+                    <Button variant='contained' size="large" color='success'> Save <Save/> </Button>
+                  </CardActions>
+                </Card>
+            </Grid>
+        </Grid>
+      </Container>
+      {/* <Stack direction="row">
+          <Box sx={
+            {
+              backgroundColor : 'primary.light',
+              height: "25%",
+              width: "25%",
+              p: 2,
+              border: '1px solid red'
+            }
+          }>
+            
+          </Box>
+      </Stack> */}
+    </>
   );
 }
 
