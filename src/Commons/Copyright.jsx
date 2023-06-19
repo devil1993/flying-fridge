32241371fcfa8a575bc7089c8d486ed328d35866
Sam Ghatak <samanwayghatak@gmail.com>
2023-06-19 12:57:09 +0000

    Fixing issue #22 - Copyright issue.

---
+++ src/Commons/Copyright.jsx
@@ -0,0 +1,21 @@
+import { Link, Typography } from "@mui/material";
+
+const Copyright = props => {
+    return (
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          align="center"
+          {...props}
+        >
+          {"Copyright © "}
+          <Link color="inherit" href="https://devil1993.github.io">
+            Samanway Ghatak - devil1993.github.io
+          </Link>{" "}
+          {new Date().getFullYear()}
+          {"."}
+        </Typography>
+      );
+}
+
+export default Copyright;
