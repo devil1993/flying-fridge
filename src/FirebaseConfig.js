d59e6e40df25335662a647f6793db4b8b6f37e87
Samanway Ghatak <samanwayghatak@gmail.com>
2023-08-01 12:13:57 +0530

    Changeing firebase deployment site.

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyB3zcVKb6WbJsugOoMJYBE2GeTS6DHU3nU",
-    authDomain: "flying-fridge.firebaseapp.com",
-    projectId: "flying-fridge",
-    storageBucket: "flying-fridge.appspot.com",
-    messagingSenderId: "709977221159",
-    appId: "1:709977221159:web:ed256516db1d14493a8602"
-  };
+  apiKey: "AIzaSyB3zcVKb6WbJsugOoMJYBE2GeTS6DHU3nU",
+  authDomain: "flying-fridge.firebaseapp.com",
+  projectId: "flying-fridge",
+  storageBucket: "flying-fridge.appspot.com",
+  messagingSenderId: "709977221159",
+  appId: "1:709977221159:web:0add6557e6d9b9cf3a8602"
+};
 
 export default firebaseConfig;
