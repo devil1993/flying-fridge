be0322c5dd2e2e10ebc4066ad51a2cefbe1d97ec
Sam Ghatak <samanwayghatak@gmail.com>
2023-08-09 10:06:31 +0000

    committing the basic script to clean orphan gratitudes.

--- jobs/clean-gratitudes.js
+++ jobs/clean-gratitudes.js
@@ -18,29 +18,39 @@ const db = getFirestore(app);
 let email = process.argv[2];
 let password = process.argv[3];
 
-signInWithEmailAndPassword(auth, email, password).then((userinfo) => {
-  console.log("UserInfo", userinfo);
+let work = async () => {
+  let userinfo = await signInWithEmailAndPassword(auth, email, password);
+
+//   console.log("UserInfo", userinfo);
+
   const userQuery = query(collection(db, "users"));
   const gratitudeQuery = query(collection(db, "gratitudes"));
   const users = [];
-  getDocs(userQuery)
-  .then((snapshot) => {
-    snapshot.forEach((user) => {
-      users.push(user.id);
-    });
-    console.log(users);
-    return getDocs(gratitudeQuery)
-  })
-  .then((snapshot) => {
-    snapshot.forEach((gratitude) => {
+  let snapshot = await getDocs(userQuery);
+
+  snapshot.forEach((user) => {
+    users.push(user.id);
+  });
+
+  console.log("Available users\n", users);
+
+  snapshot = await getDocs(gratitudeQuery);
+
+  let requests = [];
+  snapshot.forEach((gratitude) =>
+    // for(let gratitude of snapshot)
+    {
       if (!users.find((u) => u === gratitude.data().userId)) {
-        console.log("Delete", gratitude.id);
+        console.log("Deleting", gratitude.id);
         let gratitudeRef = doc(db, "gratitudes", gratitude.id);
-        deleteDoc(gratitudeRef).then((_) => {
-          console.log('deleted')
-          process.exit();
-        });
+        let p = deleteDoc(gratitudeRef);
+        requests.push(p);
       }
-    });
-  });;
-});
+    }
+  );
+  await Promise.all(requests);
+  process.exit();
+  return;
+};
+
+work();
